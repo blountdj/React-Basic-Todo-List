25e749e81c436f9a397fc4e8375a283bc04cd2d7
darren <blountdj@gmail.com>
2024-02-13 11:59:08 +0100

    final project

---
+++ README.md
@@ -0,0 +1,3 @@
+Project complete from this tutorial from Web Dev Simplified:
+
+https://www.youtube.com/watch?v=Rh3tobg7hEo
---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+import { NewTodoForm } from "./components/NewTodoForm";
+import { TodoList } from "./components/TodoList";
+
+import "./styles.css";
+
+function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue === null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  /* useEffect runs the function everytime the contents 
+     of the array in the 2nd arg change
+  */
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
+
+export default App;
